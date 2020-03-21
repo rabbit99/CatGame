84b07f05509bd97a39ead0663c7d270896ce29cf
vvbest2012 <vvbest2012@gmail.com>
2020-03-21 16:42:24 +0800

    整理資料夾

---
+++ Assets/william/Scripts/AppQuit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppQuit : MonoBehaviour
+{
+    [SerializeField] AudioClip buttonSE;
+
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        audioSource.PlayOneShot(buttonSE);
+        Invoke("AppQuitter", 0.4f);
+    }
+
+    void AppQuitter()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
